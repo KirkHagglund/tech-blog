3aa56f1568915bb9d91cd844e3658af379fbeefc
elkirko <kirkhagglund@gmail.com>
2023-02-28 18:01:28 -0600

    Added server details

---
+++ server.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+// Maybe require mysql2
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+    secret: 'Make this a random string',
+    cookie: {},
+    resave: false, //Find documentation
+    //Finish after reading
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
